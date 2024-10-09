7ffdda97309136c85f5f830465123dfcd17f2af3
dev-ukhalid <usmankhalidbackup@gmail.com>
2024-10-10 00:08:44 +0100

    image fix 2

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,13 +5,12 @@ import {
   telegram,
   twitter,
 } from "../assets";
-
-import card1 from "../assets/products/card-1.svg";
-import card2 from "../assets/products/card-2.svg";
-import card3 from "../assets/products/card-3.svg";
-import card4 from "../assets/products/card-4.svg";
-import card5 from "../assets/products/card-5.svg";
-import card6 from "../assets/products/card-6.svg";
+import { ReactComponent as card1 } from "../assets/products/card-1.svg";
+import { ReactComponent as card2 } from "../assets/products/card-2.svg";
+import { ReactComponent as card3 } from "../assets/products/card-3.svg";
+import { ReactComponent as card4 } from "../assets/products/card-4.svg";
+import { ReactComponent as card5 } from "../assets/products/card-5.svg";
+import { ReactComponent as card6 } from "../assets/products/card-6.svg";
 
 import product1 from "../assets/products/product-1.png";
 import product2 from "../assets/products/product-2.png";
@@ -19,6 +18,12 @@ import product3 from "../assets/products/product-3.png";
 import product4 from "../assets/products/product-4.png";
 import product5 from "../assets/products/product-5.png";
 import product6 from "../assets/products/product-6.png";
+import product7 from "../assets/products/product-7.png";
+import product8 from "../assets/products/product-8.png";
+import product9 from "../assets/products/product-9.png";
+import product10 from "../assets/products/product-10.png";
+import product11 from "../assets/products/product-11.png";
+import product12 from "../assets/products/product-12.png";
 
 export const navigation = [
   {
@@ -107,59 +112,86 @@ export const allProducts = [
     title: "Patient Monitor",
     text: "We offer patient monitors for various needs. Our multi-function model provides comprehensive vital signs analysis.",
     backgroundUrl: card1,
-    productImage: product1
+    productImage: product7
   },
   {
     id: "7",
     title: "High-Defination Wireless Ultrasound",
     text: "Clarius HD3 is a 30% smaller, lighter third-gen portable ultrasound with AI-powered, cloud app.",
-
-    backgroundUrl: "./src/assets/products/card-2.svg",
-    productImage: "./src/assets/products/product-2.png",
     backgroundUrl: card2,
-    productImage: product2
+    productImage: product8
   },
   {
     id: "8",
     title: "Body Composition BC",
     text: "The New Standard in Body Composition Analysis.",
-
-    backgroundUrl: "./src/assets/products/card-3.svg",
-    productImage: "./src/assets/products/product-3.png"
-  ,
     backgroundUrl: card3,
-    productImage: product3
+    productImage: product9
   },
   {
     id: "9",
     title: "Electrosurgical ZERO50",
     text: "50W RF electrosurgical unit for monopolar mode.",
-
-    backgroundUrl: "./src/assets/products/card-4.svg",
-    productImage: "./src/assets/products/product-4.png",
     backgroundUrl: card4,
-    productImage: product4
+    productImage: product10
   },
   {
     id: "10",
     title: "NAVI-60 Vein Illuminator",
     text: "NAVI-60 Vein Illuminator offers real-time vas deferens imaging, improving patient safety and reducing healthcare costs.",
-
-    backgroundUrl: "./src/assets/products/card-5.svg",
-    productImage: "./src/assets/products/product-5.png",
     backgroundUrl: card5,
-    productImage: product5
+    productImage: product11
   },
   {
     id: "11",
     title: "AV-Heart Care",
     text: "Cardiac arrest is sudden heart failure; defibrilator or CPR improves survival until help arrives.",
+    backgroundUrl: card6,
+    productImage: product12
+  },
+  {
+    id: "0",
+    title: "Patient Monitor",
+    text: "We offer patient monitors for various needs. Our multi-function model provides comprehensive vital signs analysis.",
+    backgroundUrl: card1,
+    productImage: product1
 
-    backgroundUrl: "./src/assets/products/card-6.svg",
-    productImage: "./src/assets/products/product-6.png",
+  },
+  {
+    id: "1",
+    title: "High-Defination Wireless Ultrasound",
+    text: "Clarius HD3 is a 30% smaller, lighter third-gen portable ultrasound with AI-powered, cloud app.",
+    backgroundUrl: card2,
+    productImage: product2
+  },
+  {
+    id: "2",
+    title: "Body Composition BC",
+    text: "The New Standard in Body Composition Analysis.",
+    backgroundUrl: card3,
+    productImage: product3
+  },
+  {
+    id: "3",
+    title: "Electrosurgical ZERO50",
+    text: "50W RF electrosurgical unit for monopolar mode.",
+    backgroundUrl: card4,
+    productImage: product4
+  },
+  {
+    id: "4",
+    title: "NAVI-60 Vein Illuminator",
+    text: "NAVI-60 Vein Illuminator offers real-time vas deferens imaging, improving patient safety and reducing healthcare costs.",
+    backgroundUrl: card5,
+    productImage: product5
+  },
+  {
+    id: "5",
+    title: "AV-Heart Care",
+    text: "Cardiac arrest is sudden heart failure; defibrilator or CPR improves survival until help arrives.",
     backgroundUrl: card6,
     productImage: product6
-  },
+  }
 ];
 
 export const socials = [
